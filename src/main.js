419fa5b355db0d3f9a642284b33eb8168b189319
sueno0311 <przf7ue0@gmail.com>
2022-04-23 23:28:06 +0900

    build up

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import 'es6-promise/auto'
+import App from './App.vue'
+import store from './store'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+  store
+}).$mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,8 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    port: 8080,
+    proxy: 'http://localhost:3000/'
+  }
+})
